a9535e64dce8bca6733a345904b1a8c4a4055b5b
Stephen <stevobialor@gmail.com~>
2024-07-01 01:15:37 +0100

    done

--- server.js
+++ server.js
@@ -11,18 +11,20 @@ app.set("trust proxy", true);
 app.use("/api/hello", async (req, res) => {
   const client_ip = req.ip;
   const name = req.query.visitor_name;
-  const token = process.env.TOKEN;
+  const { TOKEN, API_KEY } = process.env;
   try {
 
-    const response = await axios.get(`https://ipinfo.io/${client_ip}?token=${token}`);
+    const response = await axios.get(`https://ipinfo.io/${client_ip}?token=${TOKEN}`);
     const location = response.data.city;
-    res.status(200).json({ client_ip, location, greeting: `Hello, ${name}!, the temperature is 11 degrees Celcius in ${location}` });
+    const weather = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${response.data.loc}`);
+    const temp = weather.data.current.temp_c;
+    res.status(200).json({ client_ip, location, greeting: `Hello, ${name}!, the temperature is ${temp} degrees Celcius in ${location}` });
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ status: "error", message: "lookup api error" })
   }
 });
 
-app.listen(8080, () => {
-  console.log("server is listening on port", 8080);
+app.listen(5000, () => {
+  console.log("server is listening on port", 5000);
 });
